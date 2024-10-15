1475ff116a4b2b4b39034184e229d21dc60f59a9
Ambogo2 <ruthambogo.ra@gmail.com>
2024-10-15 12:47:13 +0300

    task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 """tasks module"""
 
+import importlib.util
 from asyncio import Task, create_task
 
-wait_random = __import__('0_basic_async_syntax').wait_random
+# Dynamically load the '0_basic_async_syntax' module
+module_name = '0_basic_async_syntax'
+module_path = './0_basic_async_syntax.py'
+
+spec = importlib.util.spec_from_file_location(module_name, module_path)
+basic_async_syntax = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(basic_async_syntax)
+
+# Access the wait_random function from the dynamically loaded module
+wait_random = basic_async_syntax.wait_random
 
 
 def task_wait_random(max_delay: int) -> Task:
-    """Tasks"""
+    """Creates and returns an asyncio task for wait_random"""
     task = create_task(wait_random(max_delay))
     return task
