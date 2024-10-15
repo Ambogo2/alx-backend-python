56e9f3e670f5251d57b897f012daf52214273171
Ambogo2 <ruthambogo.ra@gmail.com>
2024-10-15 12:44:10 +0300

    concurrent

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,20 @@
 """ multiple coroutines module"""
 import asyncio
 import heapq
-wait_random = __import__('0-basic_async_syntax').wait_random
+from typing import List
+
+# Import the wait_random function from the '0_basic_async_syntax' module
+wait_random = __import__('0_basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
-    # a list to store delays
-    delays = []
-    tasks = []
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the specified max_delay.
+    Return a sorted list of all the delays in ascending order.
+    """
+    # A list to store delays
+    delays: List[float] = []
+    tasks: List[asyncio.Task] = []
 
     # Creating and scheduling n coroutines
     for _ in range(n):
@@ -16,9 +23,9 @@ async def wait_n(n: int, max_delay: int) -> list:
         tasks.append(task)
 
     # Gathering results while maintaining a sorted list
-    for task in tasks:
+    for task in asyncio.as_completed(tasks):
         delay = await task
         heapq.heappush(delays, delay)
 
-    # Converting the heap to a sorted list
+    # Return the sorted list
     return list(delays)
